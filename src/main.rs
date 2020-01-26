4d60066c4b690fb6e0562add0ee949e7695bfe74
Jeremy <jer@jer.cx>
2020-01-26 08:28:37 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use neovim_lib::{
+    Neovim,
+    NeovimApi,
+    Session
+};
+
+use std::{
+    env::var,
+    env::args,
+};
+
+fn main() {
+
+    let socket = var("NVIM_LISTEN_ADDRESS")
+        .expect("set NVIM_LISTEN_ADDRESS environment variable");
+
+    let mut session = Session::new_unix_socket(socket).unwrap();
+    session.start_event_loop();
+    let mut nvim = Neovim::new(session);
+
+    let cmd = args()
+        .skip(1)
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    nvim.command(&cmd).unwrap();
+}
